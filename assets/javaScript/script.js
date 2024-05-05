b113d90b29190ae4dc21fc96bcc0b80f7d098cdf
esmomerr <esimomer0441@gmail.com>
2024-05-05 10:53:53 +0300

    css duzenlemeleri yapildi. Datalarda duzenleme yapildi. Fotograflarda duzenleme yapildi.

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -14,18 +14,28 @@ async function printItem(){
     data.forEach((category) => { 
         menuler.innerHTML +=
         `
-        
-<details class="details">
-            <summary class="summary">
-                <div class="cards">
-                    <div class="card">
+            <details class="details">
+                <summary class="summary">
+                    <img src="${category.backgroundImg}" alt="" class="img">
+                    <div class="deneme">
                         <h2 class="header-h2 ">${category.name}</h2>
+                    <div>
+                </summary>          
+                ${category.urunler.map(urun => 
+                    `
+                    <div class="menu">
+                        <img src="${urun.img}" alt="" class="photo">
+                        <div class="description">
+                            <div class="header-price">
+                                <h4 class="header-h4">${urun.name}</h4>
+                                <span class="price">${urun.price}₺</span>
+                            </div>
+                            <p class="paragraph">${urun.description}</p>
+                        </div>
                     </div>
-                </div>
-            </summary>          
-            ${category.urunler.map(urun => `<div class="menu"><img class="photo" src="${urun.img}" alt=""><div class="description"><div class="header-price"><div><h4>${urun.name}</h4><span>${urun.price}</span></div><p>${urun.description}</p></div></div>` )}
-</details>           
-    
+                    ` 
+                )}
+            </details>           
         `
     });
 }
